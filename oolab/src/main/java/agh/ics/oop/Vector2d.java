b77d7bab49ac30b566124931c98eb6b137a97cbb
ReptilianEye <piotr.rzadkowski@outlook.com>
2023-10-16 17:20:34 +0200

    Vector2d first functions

---
+++ oolab/src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,50 @@
+package agh.ics.oop;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return this.x;
+    }
+
+    int getY() {
+        return this.y;
+    }
+
+    public String toString() {
+        return String.format("(%d,%d)", this.x, this.y);
+    }
+
+    public boolean precedes(Vector2d other) {
+        return this.x <= other.getX() && this.getY() <= other.y;
+    }
+
+    public boolean follows(Vector2d other) {
+        return this.getX() >= other.getX() && this.getY() >= other.getY();
+    }
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.getX() + other.getX(), this.getY() + other.getY());
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.getX() - other.getX(), this.getY() - other.getY());
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(this.getX(), other.getX()), Math.max(this.getY(), other.getY()));
+    }
+    public Vector2d opposite(){
+        return new Vector2d(-this.getX(),-this.getY());
+    }
+    public boolean equals(Object other){
+        return true
+    }
+
+}
