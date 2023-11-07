cb5ea2dfb3403b1395884ffaefea1cb9ffc1186c
Piotr Rzadkowski <81797728+ReptilianEye@users.noreply.github.com>
2023-11-07 12:23:11 +0000

    tests2

--- oolab/src/test/java/agh/ics/oop/SimulationTest.java
+++ oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -12,8 +12,9 @@ class SimulationTest {
 
     @Test
     void basicCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"});
+        // given
+        List<MoveDirection> Orders = OptionsParser
+                .parse(new String[] { "f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f" });
         List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
         int animalsCount = startingPositions.size();
         WorldMap map = new RectangularMap(4, 4);
@@ -25,8 +26,8 @@ class SimulationTest {
         Vector2d expectedAnimal2P = new Vector2d(2, 4);
         MapDirection expectedAnimal2O = MapDirection.NORTH;
 
-        //when
-//        int actualAnimalsPlaced = simulation.placeAnimals();
+        // when
+        // int actualAnimalsPlaced = simulation.placeAnimals();
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
@@ -36,8 +37,9 @@ class SimulationTest {
         Vector2d actualAnimal2P = animalsAfter.get(1).getPosition();
         MapDirection actualAnimal2O = animalsAfter.get(1).getOrientation();
 
-        //then
-//        Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals should be placed if had different starting positions");
+        // then
+        // Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals
+        // should be placed if had different starting positions");
 
         Assertions.assertEquals(expectedAnimal1P, actualAnimal1P);
         Assertions.assertEquals(expectedAnimal1O, actualAnimal1O);
@@ -49,9 +51,12 @@ class SimulationTest {
 
     @Test
     void manyAnimalsLittleOrdersCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f"});
-        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10), new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17), new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7), new Vector2d(11, 13));
+        // given
+        List<MoveDirection> Orders = OptionsParser.parse(new String[] { "f", "b", "r", "l", "f" });
+        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10),
+                new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17),
+                new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7),
+                new Vector2d(11, 13));
         int animalsCount = startingPositions.size();
         WorldMap map = new RectangularMap(19, 19);
         Simulation simulation = new Simulation(Orders, startingPositions, map);
@@ -71,8 +76,8 @@ class SimulationTest {
         Vector2d expectedAnimal5P = new Vector2d(1, 0);
         MapDirection expectedAnimal5O = MapDirection.NORTH;
 
-        //when
-//        int actualAnimalsPlaced = simulation.placeAnimals();
+        // when
+        // int actualAnimalsPlaced = simulation.placeAnimals();
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
@@ -91,8 +96,9 @@ class SimulationTest {
         Vector2d actualAnimal5P = animalsAfter.get(4).getPosition();
         MapDirection actualAnimal5O = animalsAfter.get(5).getOrientation();
 
-        //then
-//        Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals should be placed if had different starting positions");
+        // then
+        // Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals
+        // should be placed if had different starting positions");
 
         Assertions.assertEquals(expectedAnimal1P, actualAnimal1P);
         Assertions.assertEquals(expectedAnimal1O, actualAnimal1O);
@@ -106,13 +112,15 @@ class SimulationTest {
         Assertions.assertEquals(expectedAnimal4P, actualAnimal4P);
         Assertions.assertEquals(expectedAnimal4O, actualAnimal4O);
 
-        Assertions.assertEquals(expectedAnimal5P, actualAnimal5P, "Animal should not move if after move it would enter on others animals field");
+        Assertions.assertEquals(expectedAnimal5P, actualAnimal5P,
+                "Animal should not move if after move it would enter on others animals field");
         Assertions.assertEquals(expectedAnimal5O, actualAnimal5O);
 
-
         for (int i = Orders.size(); i < animalsAfter.size(); i++) {
-            Assertions.assertEquals(startingPositions.get(i), animalsAfter.get(i).getPosition(), "Position of unmoved animal " + i + " should remain the same");
-            Assertions.assertEquals(MapDirection.NORTH, animalsAfter.get(i).getOrientation(), "Orientation of unmoved animal " + i + " should remain the same");
+            Assertions.assertEquals(startingPositions.get(i), animalsAfter.get(i).getPosition(),
+                    "Position of unmoved animal " + i + " should remain the same");
+            Assertions.assertEquals(MapDirection.NORTH, animalsAfter.get(i).getOrientation(),
+                    "Orientation of unmoved animal " + i + " should remain the same");
         }
         System.out.println(map);
 
@@ -120,52 +128,54 @@ class SimulationTest {
 
     @Test
     void manyAnimalsManyOrdersCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f", "f", "b", "f", "f", "l", "l", "r", "b", "b", "l", "b", "r", "r", "b", "f", "r", "b", "l", "b", "b", "b", "r", "b", "l", "b", "r", "r", "f", "l", "f", "f", "l", "b", "l", "b", "b", "f", "l", "b", "r", "l", "f", "f", "f", "f", "b", "r", "b", "f", "f"});
-        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10), new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17), new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7), new Vector2d(11, 13));
+        // given
+        List<MoveDirection> Orders = OptionsParser.parse(new String[] { "f", "b", "r", "l", "f", "f", "r", "r", "f",
+                "f", "f", "f", "f", "f", "f", "f", "f", "b", "f", "f", "l", "l", "r", "b", "b", "l", "b", "r", "r", "b",
+                "f", "r", "b", "l", "b", "b", "b", "r", "b", "l", "b", "r", "r", "f", "l", "f", "f", "l", "b", "l", "b",
+                "b", "f", "l", "b", "r", "l", "f", "f", "f", "f", "b", "r", "b", "f", "f" });
+        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10),
+                new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17),
+                new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7),
+                new Vector2d(11, 13));
         int animalsCount = startingPositions.size();
         WorldMap map = new RectangularMap(19, 19);
         Simulation simulation = new Simulation(Orders, startingPositions, map);
 
-        List<Vector2d> expectedPositions = List.of(new Vector2d(0, 3), new Vector2d(2, 3), new Vector2d(8, 10), new Vector2d(0, 1), new Vector2d(1, 0), new Vector2d(15, 7), new Vector2d(6, 14), new Vector2d(4, 17), new Vector2d(3, 11), new Vector2d(13, 10), new Vector2d(14, 9), new Vector2d(5, 8), new Vector2d(12, 14));
-        List<MapDirection> expectedDirections = List.of(MapDirection.WEST, MapDirection.NORTH, MapDirection.WEST, MapDirection.EAST, MapDirection.WEST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST, MapDirection.SOUTH, MapDirection.EAST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST);
+        List<Vector2d> expectedPositions = List.of(new Vector2d(0, 3), new Vector2d(2, 3), new Vector2d(8, 10),
+                new Vector2d(0, 1), new Vector2d(1, 0), new Vector2d(15, 7), new Vector2d(6, 14), new Vector2d(4, 17),
+                new Vector2d(3, 11), new Vector2d(13, 10), new Vector2d(14, 9), new Vector2d(5, 8),
+                new Vector2d(12, 14));
+        List<MapDirection> expectedDirections = List.of(MapDirection.WEST, MapDirection.NORTH, MapDirection.WEST,
+                MapDirection.EAST, MapDirection.WEST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST,
+                MapDirection.SOUTH, MapDirection.EAST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST);
 
-        //when
+        // when
         int actualAnimalsPlaced = simulation.placeAnimals();
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
-
-        //then
-        Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals should be placed if had different starting positions");
+        // then
+        Assertions.assertEquals(animalsCount, actualAnimalsPlaced,
+                "All animals should be placed if had different starting positions");
 
         for (int i = 0; i < expectedPositions.size(); i++) {
             Animal current = animalsAfter.get(i);
-            Assertions.assertEquals(expectedPositions.get(i), current.getPosition(), "Animal " + i + " position is wrong");
-            Assertions.assertEquals(expectedDirections.get(i), current.getOrientation(), "Animal " + i + " orientation is wrong");
+            Assertions.assertEquals(expectedPositions.get(i), current.getPosition(),
+                    "Animal " + i + " position is wrong");
+            Assertions.assertEquals(expectedDirections.get(i), current.getOrientation(),
+                    "Animal " + i + " orientation is wrong");
         }
         System.out.println(map);
     }
-}
-package agh.ics.oop;
-
-import agh.ics.oop.model.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
-
-class SimulationTest {
 
     @Test
-    void basicCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"});
+    void basicCaseFieldMap() {
+        // given
+        List<MoveDirection> Orders = OptionsParser
+                .parse(new String[] { "f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f" });
         List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
         int animalsCount = startingPositions.size();
-        WorldMap map = new FieldMap(4, 4);
+        WorldMap map = new FieldMap(4);
         Simulation simulation = new Simulation(Orders, startingPositions, map);
 
         Vector2d expectedAnimal1P = new Vector2d(3, 0);
@@ -174,7 +184,7 @@ class SimulationTest {
         Vector2d expectedAnimal2P = new Vector2d(2, 4);
         MapDirection expectedAnimal2O = MapDirection.NORTH;
 
-        //when
+        // when
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
@@ -184,7 +194,7 @@ class SimulationTest {
         Vector2d actualAnimal2P = animalsAfter.get(1).getPosition();
         MapDirection actualAnimal2O = animalsAfter.get(1).getOrientation();
 
-        //then
+        // then
         Assertions.assertEquals(expectedAnimal1P, actualAnimal1P);
         Assertions.assertEquals(expectedAnimal1O, actualAnimal1O);
         Assertions.assertEquals(expectedAnimal2P, actualAnimal2P);
@@ -194,12 +204,15 @@ class SimulationTest {
     }
 
     @Test
-    void manyAnimalsLittleOrdersCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f"});
-        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10), new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17), new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7), new Vector2d(11, 13));
+    void manyAnimalsLittleOrdersCaseFieldMap() {
+        // given
+        List<MoveDirection> Orders = OptionsParser.parse(new String[] { "f", "b", "r", "l", "f" });
+        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10),
+                new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17),
+                new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7),
+                new Vector2d(11, 13));
         int animalsCount = startingPositions.size();
-        WorldMap map = new FieldMap(19, 19);
+        WorldMap map = new FieldMap(19);
         Simulation simulation = new Simulation(Orders, startingPositions, map);
 
         Vector2d expectedAnimal1P = new Vector2d(2, 3);
@@ -217,7 +230,7 @@ class SimulationTest {
         Vector2d expectedAnimal5P = new Vector2d(1, 0);
         MapDirection expectedAnimal5O = MapDirection.NORTH;
 
-        //when
+        // when
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
@@ -236,7 +249,7 @@ class SimulationTest {
         Vector2d actualAnimal5P = animalsAfter.get(4).getPosition();
         MapDirection actualAnimal5O = animalsAfter.get(5).getOrientation();
 
-        //then
+        // then
         Assertions.assertEquals(expectedAnimal1P, actualAnimal1P);
         Assertions.assertEquals(expectedAnimal1O, actualAnimal1O);
 
@@ -249,43 +262,58 @@ class SimulationTest {
         Assertions.assertEquals(expectedAnimal4P, actualAnimal4P);
         Assertions.assertEquals(expectedAnimal4O, actualAnimal4O);
 
-        Assertions.assertEquals(expectedAnimal5P, actualAnimal5P, "Animal should not move if after move it would enter on others animals field");
+        Assertions.assertEquals(expectedAnimal5P, actualAnimal5P,
+                "Animal should not move if after move it would enter on others animals field");
         Assertions.assertEquals(expectedAnimal5O, actualAnimal5O);
 
-
         for (int i = Orders.size(); i < animalsAfter.size(); i++) {
-            Assertions.assertEquals(startingPositions.get(i), animalsAfter.get(i).getPosition(), "Position of unmoved animal " + i + " should remain the same");
-            Assertions.assertEquals(MapDirection.NORTH, animalsAfter.get(i).getOrientation(), "Orientation of unmoved animal " + i + " should remain the same");
+            Assertions.assertEquals(startingPositions.get(i), animalsAfter.get(i).getPosition(),
+                    "Position of unmoved animal " + i + " should remain the same");
+            Assertions.assertEquals(MapDirection.NORTH, animalsAfter.get(i).getOrientation(),
+                    "Orientation of unmoved animal " + i + " should remain the same");
         }
         System.out.println(map);
 
     }
 
     @Test
-    void manyAnimalsManyOrdersCase() {
-        //given
-        List<MoveDirection> Orders = OptionsParser.parse(new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f", "f", "b", "f", "f", "l", "l", "r", "b", "b", "l", "b", "r", "r", "b", "f", "r", "b", "l", "b", "b", "b", "r", "b", "l", "b", "r", "r", "f", "l", "f", "f", "l", "b", "l", "b", "b", "f", "l", "b", "r", "l", "f", "f", "f", "f", "b", "r", "b", "f", "f"});
-        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10), new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17), new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7), new Vector2d(11, 13));
+    void manyAnimalsManyOrdersCaseFieldMap() {
+        // given
+        List<MoveDirection> Orders = OptionsParser.parse(new String[] { "f", "b", "r", "l", "f", "f", "r", "r", "f",
+                "f", "f", "f", "f", "f", "f", "f", "f", "b", "f", "f", "l", "l", "r", "b", "b", "l", "b", "r", "r", "b",
+                "f", "r", "b", "l", "b", "b", "b", "r", "b", "l", "b", "r", "r", "f", "l", "f", "f", "l", "b", "l", "b",
+                "b", "f", "l", "b", "r", "l", "f", "f", "f", "f", "b", "r", "b", "f", "f" });
+        List<Vector2d> startingPositions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(6, 10),
+                new Vector2d(1, 1), new Vector2d(1, 0), new Vector2d(15, 4), new Vector2d(3, 14), new Vector2d(6, 17),
+                new Vector2d(2, 11), new Vector2d(16, 9), new Vector2d(14, 8), new Vector2d(7, 7),
+                new Vector2d(11, 13));
         int animalsCount = startingPositions.size();
-        WorldMap map = new FieldMap(19, 19);
+        WorldMap map = new FieldMap(19);
         Simulation simulation = new Simulation(Orders, startingPositions, map);
 
-        List<Vector2d> expectedPositions = List.of(new Vector2d(0, 3), new Vector2d(2, 3), new Vector2d(8, 10), new Vector2d(0, 1), new Vector2d(1, 0), new Vector2d(15, 7), new Vector2d(6, 14), new Vector2d(4, 17), new Vector2d(3, 11), new Vector2d(13, 10), new Vector2d(14, 9), new Vector2d(5, 8), new Vector2d(12, 14));
-        List<MapDirection> expectedDirections = List.of(MapDirection.WEST, MapDirection.NORTH, MapDirection.WEST, MapDirection.EAST, MapDirection.WEST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST, MapDirection.SOUTH, MapDirection.EAST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST);
+        List<Vector2d> expectedPositions = List.of(new Vector2d(0, 3), new Vector2d(2, 3), new Vector2d(8, 10),
+                new Vector2d(0, 1), new Vector2d(1, 0), new Vector2d(15, 7), new Vector2d(6, 14), new Vector2d(4, 17),
+                new Vector2d(3, 11), new Vector2d(13, 10), new Vector2d(14, 9), new Vector2d(5, 8),
+                new Vector2d(12, 14));
+        List<MapDirection> expectedDirections = List.of(MapDirection.WEST, MapDirection.NORTH, MapDirection.WEST,
+                MapDirection.EAST, MapDirection.WEST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST,
+                MapDirection.SOUTH, MapDirection.EAST, MapDirection.NORTH, MapDirection.EAST, MapDirection.WEST);
 
-        //when
+        // when
         int actualAnimalsPlaced = simulation.placeAnimals();
         simulation.run();
         List<Animal> animalsAfter = simulation.getAnimals();
 
-
-        //then
-        Assertions.assertEquals(animalsCount, actualAnimalsPlaced, "All animals should be placed if had different starting positions");
+        // then
+        Assertions.assertEquals(animalsCount, actualAnimalsPlaced,
+                "All animals should be placed if had different starting positions");
 
         for (int i = 0; i < expectedPositions.size(); i++) {
             Animal current = animalsAfter.get(i);
-            Assertions.assertEquals(expectedPositions.get(i), current.getPosition(), "Animal " + i + " position is wrong");
-            Assertions.assertEquals(expectedDirections.get(i), current.getOrientation(), "Animal " + i + " orientation is wrong");
+            Assertions.assertEquals(expectedPositions.get(i), current.getPosition(),
+                    "Animal " + i + " position is wrong");
+            Assertions.assertEquals(expectedDirections.get(i), current.getOrientation(),
+                    "Animal " + i + " orientation is wrong");
         }
         System.out.println(map);
     }
