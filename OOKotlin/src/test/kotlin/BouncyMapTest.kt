d4ae3e8135ef6d9df98fd70350880f3183e9b8e9
Piotr Rzadkowski <81797728+ReptilianEye@users.noreply.github.com>
2023-12-15 19:35:35 +0000

    tests

--- OOKotlin/src/test/kotlin/BouncyMapTest.kt
+++ OOKotlin/src/test/kotlin/BouncyMapTest.kt
@@ -1,16 +1,53 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 class BouncyMapTest : FunSpec({
 
     test("place") {
+        val map = BouncyMap(5, 5)
+        val animal = Animal(Vector2d(2, 2))
+        val animal2 = Animal(Vector2d(2, 2))
+
+        map.place(animal)
+        map.objectAt(Vector2d(2, 2)) shouldBe animal
+        map.place(animal2)
+        
     }
 
-    test("move") { }
+    test("move") {
+        val map = BouncyMap(5, 5)
+        val animal = Animal(Vector2d(2, 2))
 
-    test("isOccupied") { }
+        map.place(animal)
+        map.move(animal, MoveDirection.FORWARD)
 
-    test("objectAt") { }
+        val newPosition = animal.getPosition()
+        newPosition shouldNotBe Vector2d(2, 2)
+        map.objectAt(newPosition) shouldBe animal
+    }
 
-    test("canMoveTo") { }
+    test("isOccupied") {
+        val map = BouncyMap(5, 5)
+        val animal = Animal(Vector2d(2, 2))
+
+        map.place(animal)
+        map.isOccupied(Vector2d(2, 2)) shouldBe true
+    }
+
+    test("objectAt") {
+        val map = BouncyMap(5, 5)
+        val animal = Animal(Vector2d(2, 2))
+
+        map.place(animal)
+        map.objectAt(Vector2d(2, 2)) shouldBe animal
+    }
+
+    test("canMoveTo") {
+        val map = BouncyMap(5, 5)
+        val animal = Animal(Vector2d(2, 2))
+
+        map.place(animal)
+        map.canMoveTo(Vector2d(3, 3)) shouldBe true
+    }
 })
